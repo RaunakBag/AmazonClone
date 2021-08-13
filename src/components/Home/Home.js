7d75464888d85b961b5874f6ec0bf5221c384dfd
RaunakBag <raunakbag97@gmail.com>
2021-08-13 20:09:52 +0530

    Slight changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,7 @@ function Home() {
             id="12321341"
             title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
             price={189.99}
-            rating={5}
+            rating={4}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
           />
           <Product
